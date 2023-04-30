1fbcd3ab3621e0e35b9c055fcffe9f2d7de94716
moeyanhtun <moeyan227970@gmail.com>
2023-04-30 18:36:50 +0630

    Html & css

---
+++ javascript /app.js
@@ -0,0 +1,28 @@
+// let currentAge = 18
+
+// function getBirthYear(){
+//     let currentDate = 2023 - currentAge;
+//     let result = `ကျွန်တော်သည် ${currentDate} တွင်ဖွားမြင်ခဲ့သည်။`
+//     return result;
+// }
+// let fruit = "grap";
+// if(fruit == "Mango"){
+//     console.log("This is Mango");
+// }else if (fruit == "apple"){
+//     console.log("this is apple");
+// }else if(fruit == "Orange"){
+//     console.log("this is orange");
+// }else if(fruit == "strawbarry"){
+//     console.log("This is strawbarry");
+// }else{
+//     console.log(
+//         "this is other fruit"
+//     );
+// }                                                                                          
+// console.log("the end");
+
+let userName = prompt("Enter the your name");
+let age = prompt("Enter the age");
+switch(userName,age){
+    case userName,age: console.log(`He is ${userName} and He is ${age} years old`);
+}
